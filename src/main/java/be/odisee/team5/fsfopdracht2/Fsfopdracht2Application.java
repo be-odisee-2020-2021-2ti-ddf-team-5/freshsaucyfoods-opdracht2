d1becfbcbccc95b9cf171fa0175ff2fd80afbad7
BramVanPevenage <55691795+BramVanPevenage@users.noreply.github.com>
2021-03-22 15:25:09 +0100

    opstart opdracht

---
+++ src/main/java/be/odisee/team5/fsfopdracht2/Fsfopdracht2Application.java
@@ -0,0 +1,13 @@
+package be.odisee.team5.fsfopdracht2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsfopdracht2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fsfopdracht2Application.class, args);
+    }
+
+}
---
+++ src/main/java/be/odisee/team5/fsfopdracht2/HomeController.java
@@ -0,0 +1,12 @@
+package be.odisee.team5.fsfopdracht2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
